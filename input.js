7744f7c832823f023c3f37fa7c935ec677bfe7d4
Akshat <akshatsaraswat03@gmail.com>
2020-10-16 08:29:24 +0530

    tried something

--- input.js
+++ input.js
@@ -9,7 +9,7 @@ window.addEventListener('keydown', e => {
       break
     case 'ArrowDown':
       if (lastInputDirection.y !== 0) break
-      inputDirection = { x: 0, y: 1 }
+      inputDirection = { x:0, y:1 }
       break
     case 'ArrowLeft':
       if (lastInputDirection.x !== 0) break
